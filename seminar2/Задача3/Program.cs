ab0e2d514569860aea0940bedd07c74cb8ccf7da
Maxim <maks.meshcheryakov.93@mail.ru>
2023-02-04 23:51:27 +0300

    Второе задание выполнено

---
+++ seminar2/Задача3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Введите номер дня");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>0&&num<8){
+    if(num == 6 || num ==7){
+    Console.WriteLine("Да");
+}
+else {
+     Console.WriteLine("Нет");
+}
+
+}
+else{
+    Console.WriteLine ("Такого дня нет");
+}
