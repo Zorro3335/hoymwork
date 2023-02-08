eb4e8d0f6851ce505298a8d0b9e13aa7c1229dd0
Maxim <maks.meshcheryakov.93@mail.ru>
2023-02-08 22:32:15 +0300

    вторая задача выполнена

--- Seminar3/Задача2/Program.cs
+++ Seminar3/Задача2/Program.cs
@@ -1,14 +1,30 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Программа по нахождению координат двух точек в 3D пространстве");
+
+Console.Write("Введите x1 -> ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1 -> ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1 -> ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x2 -> ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2 -> ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z2 ->");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
 
-Console.Write("Введите число -> ");
-int num = Convert.ToInt32(Console.ReadLine());
+double x = Math.Pow(x2 - x1, 2);
+double y = Math.Pow(y2 - y1, 2);
+double z = Math.Pow(z2 - z1, 2);
 
-Console.WriteLine("Таблица кубов");
+double ab = Math.Round(Math.Sqrt(x + y + z), 2);
 
-for (int i = 1; i <= num; i++){
-    Console.WriteLine($"{i} в кубе равно -> {Math.Pow(i, 3)}");
-}
+Console.WriteLine($"Растояние между точка А{x1} {y1} {z1} и B{x2} {y2} {z2} равно {ab}");
